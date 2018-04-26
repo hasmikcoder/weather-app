9c5fa09b7651ccb8cd5cb321d4244c5d04c7532d
Your Name <you@example.com>
2018-04-26 14:31:17 -0500

    Added API KEY

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather.js";
 
+
+const API_KEY = "a994c811642c4c8a4bfa3c449c2d8201"
+
 class App extends React.Component {
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/
+      weather?q=London,uk&appid= ${API_KEY}`);
+      const data = await api_call.json();
+      console.log(data);
+  }
 
    render(){
      return (
