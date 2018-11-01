a3a27a7b138b211b64315b25e83e42dfc032bc98
Your Name <you@example.com>
2018-11-01 12:30:35 -0500

    Updated Form component

--- src/App.js
+++ src/App.js
@@ -9,33 +9,35 @@ const API_KEY = "a994c811642c4c8a4bfa3c449c2d8201";
 class App extends React.Component {
 
   state = {
-    temperature: undefined,
-    city: undefined,
-    country: undefined,
-    humidity: undefined,
-    description: undefined,
-    error: undefined,
+  //  temperature: undefined,
+    //city: undefined,
+    //country: undefined,
+    //humidity: undefined,
+    //description: undefined,
+    //error: undefined,
 
   }
 
   getWeather = async (e) => {
     e.preventDefault();
-    const city = e.target.elements.city.value;
-    const country = e.target.elements.country.value;
+    const city = e.target.elements.name.city.value;
+    const country = e.target.elements.name.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/
       weather?q=${city},${country}&appid= ${API_KEY}&units=metric`);
       const data = await api_call.json();
+      if (city && country)
       console.log(data);
       this.setState({
-        // temperature: data.main.temp,
-        // city: data.name;
-        // country: data.sys.country,
-        // humidity: data.main.humidity,
-        // description: data.weather[0].description,
-        // error: ""
+         temperature: undefined,
+         city: undefined;
+         country: undefined,
+         humidity: undefined,
+         description: undefined,
+        error: "Please enter the value."
 
       });
   }
+}
 
    render(){
      return (
@@ -53,6 +55,6 @@ class App extends React.Component {
        </div>
      );
    }
- }
+ };
 
  export default App;
